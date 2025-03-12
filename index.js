417b3866107d88e23805d937c2e09d6e8f93760b
Pratham Mittal <prathamm4402@gmail.com>
2025-03-12 10:31:47 +0530

    Set up backend with MongoDB connection

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+require('dotenv').config(); // Load environment variables
 const express = require('express');
+const mongoose = require('mongoose');
 const { resolve } = require('path');
 
 const app = express();
 const port = 3010;
+
+
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to database'))
+  .catch((err) => console.error('Error connecting to database:', err));
 
 app.use(express.static('static'));
 
@@ -11,5 +19,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
